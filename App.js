0350917240b0ddfcb28f705bb4118405ef109fae
Will-Peterson <william79904@yahoo.com>
2018-09-01 18:53:23 -0700

    update

--- App.js
+++ App.js
@@ -14,15 +14,39 @@ class AuctionDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: this.props.navigation.state.params.item.auction_id
+      year: this.props.navigation.state.params.item.year,
+      month: this.props.navigation.state.params.item.month,
+      day: this.props.navigation.state.params.item.day,
+      time: this.props.navigation.state.params.item.time,
+      storage_facility: this.props.navigation.state.params.item.storage_facility,
+      address: this.props.navigation.state.params.item.address,
+      city: this.props.navigation.state.params.item.city,
+      state: this.props.navigation.state.params.item.state,
+      phone: this.props.navigation.state.params.item.phone,
+      auctioneer: this.props.navigation.state.params.item.auctioneer
     }
   }
   render() {
-
     return (
       <View>
-        <Text style={{fontSize: 25, justifyContent: 'center', alignItems: 'center'}}>
-          {this.state.id}
+        <Text style={{fontSize: 27, color: 'blue', fontWeight: 'bold'}}>
+          {this.state.time + '\n'}
+        </Text>
+        <Text style={{fontSize: 30, fontWeight: 'bold'}}>
+          {this.state.storage_facility + '\n'}
+        </Text>
+        <TouchableOpacity>
+          <Text style={{fontSize: 27}}>
+            {this.state.address + '\n' + this.state.city + ', ' + this.state.state + '\n'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={{fontSize: 25}}>
+            {this.state.phone}
+          </Text>
+        </TouchableOpacity>
+        <Text>
+          {'\n\n\nAuctioneer: ' + this.state.auctioneer}
         </Text>
       </View>
     );
